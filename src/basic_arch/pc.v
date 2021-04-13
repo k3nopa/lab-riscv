d16a789ad166fb1c9f9644ee9fbd95389ec9cb60
k3nopa <afiqnaufalpro@gmail.com>
2021-04-13 11:29:08 +0900

    add tests dir and pc module

---
+++ src/basic_arch/pc.v
@@ -0,0 +1,19 @@
+module pc(clock,reset,pcin,pcwrite,pcout);
+
+  input clock,reset;
+  input [31:0] pcin;
+  input 	pcwrite;
+  output[31:0] pcout;
+  reg[31:0] 	pcout;
+
+
+  always@(posedge clock or negedge reset)
+    begin
+
+    if(reset==1'b0)
+    begin
+      pcout<=32'h00010000;
+    end
+  end
+
+endmodule // pc
