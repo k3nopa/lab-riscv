e68f0a418be0c754038dd0f0cc1868bba29ad0c3
k3nopa <afiqnaufalpro@gmail.com>
2021-04-28 10:23:30 +0900

    add test module for reg32

---
+++ tests/test_reg32.v
@@ -0,0 +1,30 @@
+module test_reg32 ();
+  reg reset,
+  reg clk,
+  reg [31:0] d,
+
+  wire [31:0] q
+
+  reg32 register(.reset(reset), .clk(clk), .d(d), .q(q));
+
+  always begin
+    clk = 1'b1;
+    #5 clk = 1'b0;
+    #5;
+  end
+
+  initial begin
+    reset = 1;
+    d = 1;
+    #20
+
+    d = 1;
+    #10
+    d = 0;
+    #10
+    d = 1;
+    #10
+
+    $finish
+  end
+endmodule
