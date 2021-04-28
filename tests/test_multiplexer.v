1322a021583bdf60b858df11c100f15f39382638
k3nopa <afiqnaufalpro@gmail.com>
2021-04-28 12:23:52 +0900

    fixed grammar error

--- tests/test_multiplexer.v
+++ tests/test_multiplexer.v
@@ -1,9 +1,9 @@
 module test_multiplexer ();
-  reg [31:0] in1,
-  reg [31:0] in2,
-  reg select,
+  reg [31:0] in1;
+  reg [31:0] in2;
+  reg select;
 
-  wire [31:0] out
+  wire [31:0] out;
 
   Mux2_1 mux(.in1(in1), .in2(in2), .select(select), .out(out));
 
