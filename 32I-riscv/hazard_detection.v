46f52810d19e1b54007e01d100a341a3ebe391ff
k3nopa <afiqnaufalpro@gmail.com>
2021-05-27 00:21:26 +0900

    remove old hazard detection

--- 32I-riscv/hazard_detection.v
+++ 32I-riscv/hazard_detection.v
@@ -61,68 +61,12 @@ module hazard_detection(
                     end
                 end
             end
+            else
+                hazard_check = {1'b0, 3'b000, 1'b0};
         end
+
     endfunction
 
     assign {hazard, stall} = hazard_check(current, before);
 
 endmodule
-
-//module hazard_detection(
-//    input [31:0] id_inst, ex_inst, mem_inst,
-//
-//    output [3:0] hazard,
-//    output stall
-//);
-//
-//    localparam [2:0]
-//    NONE = 0,
-//    FROM_EX_RS1 = 1,
-//    FROM_EX_RS2 = 2,
-//    FROM_MEM_RS1 = 3,
-//    FROM_MEM_RS2 = 4;
-//
-//    // Instructions' Type
-//    localparam [6:0]
-//    LUI = 7'b0110111,
-//    AUIPC = 7'b0010111,
-//    LOAD = 7'b0000011,
-//    JAL = 7'b1101111;
-//
-//    function [4:0] hazard_check(input [31:0] _id_inst, _ex_inst, _mem_inst);
-//        begin
-//            if (_id_inst[6:0] !== LUI || _id_inst[6:0] !== AUIPC || _id_inst[6:0] !== JAL) begin
-//                
-//                // compare current inst at id to inst at mem(if inst exist)
-//                if (_mem_inst !== 32'hx && _mem_inst[11:7] === _id_inst[19:15])
-//                    hazard_check = {1'b1, FROM_MEM_RS1, 1'b0};
-//                else if (_mem_inst !== 32'hx && _mem_inst[11:7] === _id_inst[24:20])
-//                    hazard_check = {1'b1, FROM_MEM_RS2, 1'b0};
-//
-//                // compare current inst at id to inst at ex(if inst exist)
-//                else if (_ex_inst !== 32'hx && _ex_inst[11:7] === _id_inst[19:15]) begin
-//                    // if load was instruction before, stall current stage
-//                    if (_ex_inst[6:0] === LOAD) begin
-//                        hazard_check = {1'b1, FROM_EX_RS1, 1'b1};
-//                    end
-//                    else begin
-//                        hazard_check = {1'b1, FROM_EX_RS1, 1'b0};
-//                    end
-//                end
-//                else if (_ex_inst !== 32'hx && _ex_inst[11:7] === _id_inst[24:20]) begin
-//                    // if load was instruction before, stall current stage
-//                    if (_ex_inst[6:0] === LOAD) begin
-//                        hazard_check = {1'b1, FROM_EX_RS2, 1'b1};
-//                    end
-//                    else begin
-//                        hazard_check = {1'b1, FROM_EX_RS2, 1'b0};
-//                    end
-//                end
-//
-//            end
-//        end
-//    endfunction
-//
-//    assign {hazard, stall} = hazard_check(id_inst, ex_inst, mem_inst);
-//
-//endmodule
