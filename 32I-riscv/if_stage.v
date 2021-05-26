4c518be4a7503b9a6d3ade0bf71c47f1fc7097ed
k3nopa <afiqnaufalpro@gmail.com>
2021-05-27 00:29:02 +0900

    handling stalling from if stage

--- 32I-riscv/if_stage.v
+++ 32I-riscv/if_stage.v
@@ -1,6 +1,7 @@
 module if_stage (
     input reset,    // active at low
     input pc_src,
+    input stall,
     input [1:0] jump,
     input [31:0] pc,
     input [31:0] branch_addr,
@@ -10,15 +11,19 @@ module if_stage (
     output [31:0] inst_addr
 );
 
-    wire [31:0] current_pc;
+    wire [31:0] current_pc, new_pc;
     
     multiplexer pc_mux(
         .in1(pc), .in2(branch_addr), .in3(jump_addr), .select(pc_src), .jump(jump),
         .out(current_pc)
     );
 
-    if_pc_adder pc_adder(.reset(reset), .pc(current_pc), .pc4(pc4));
-    
-    assign inst_addr = current_pc;
+    if_pc_adder pc_adder(.reset(reset), .pc(current_pc), .pc4(new_pc));
+        
+        
+    // need to minus 4 because, stall is detected during id stage.
+    // at this stage, pc is already at next instruction
+    assign pc4 = (stall !== 1'bx && stall === 1'b1) ? (pc - 32'd4) : new_pc;
+    assign inst_addr = (stall !== 1'bx && stall === 1'b1) ? pc : current_pc;
 
 endmodule
