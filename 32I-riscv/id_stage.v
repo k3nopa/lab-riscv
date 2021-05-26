274672d7ce016a32e1e1762c109a0a738f8ce1e0
k3nopa <afiqnaufalpro@gmail.com>
2021-05-27 00:26:24 +0900

    changes due to new output from control module

--- 32I-riscv/id_stage.v
+++ 32I-riscv/id_stage.v
@@ -13,18 +13,22 @@ module id_stage (
     wire mem_read, mem_write, alu_src_a, alu_src_b, reg_write;
     wire [1:0] mem_to_reg, jump;
     wire [3:0] alu_op;
+    wire [4:0] shift_amount;
+    wire [31:0] extend_imm;
     
     /* ----- Control Unit ----- */
     id_control control_unit(
         .reset(reset), .inst(inst),
         .mem_read(mem_read), .mem_write(mem_write), .reg_write(reg_write),
-        .alu_src_a(alu_src_a), .alu_src_b(alu_src_b), .mem_to_reg(mem_to_reg), .jump(jump), .inst_size(inst_size), .is_signed(is_signed), .alu_op(alu_op)
+        .alu_src_a(alu_src_a), .alu_src_b(alu_src_b), .mem_to_reg(mem_to_reg), .jump(jump), 
+        .inst_size(inst_size), .is_signed(is_signed), .alu_op(alu_op), .shift_amount(shift_amount)
     );
     
-    id_sign_extend imm_extend(.inst(inst), .extend_imm(sign_extend));
+    id_sign_extend imm_extend(.inst(inst), .extend_imm(extend_imm));
 
     /* ----- Address Adder ----- */
-    assign branch_addr = pc + sign_extend;
+    assign sign_extend = (shift_amount !== 5'hx) ? shift_amount : extend_imm;
+    assign branch_addr = pc + extend_imm;
 
     assign controls = {mem_read, mem_write, alu_src_a, alu_src_b, mem_to_reg, alu_op, reg_write, jump};
 
