800c3a46a705ca2d721215a3b65559c23230eb45
k3nopa <afiqnaufalpro@gmail.com>
2021-05-28 14:30:50 +0900

    test file for debugging jalr

--- test_pack/asm/skel/test.s
+++ test_pack/asm/skel/test.s
@@ -1,16 +1,33 @@
 	.text    # text section
 main:
-	li	x1,	0x2
-	li	x1,	0x3
-	jal	x3,	calc
+	li	x16,	0x80010000
+	li	x10,	0x0
+	li	x10,	0x5
+	li	x10,	0x3
+	lw	x11,	0x0(x16)
+
+	j	L1
+
+	sb	x10,	0x4(x16)
+	nop
+L1:
+	jal	L2
+	sb	x10,	0x8(x16)
+	ret
+L2:
+	ori	x10,	x10,	0x3
+	jalr	x1
+L3:
+	xor	x8,	x8,	x8
+	addi	x8,	x8,	0x03
+	xor	x9,	x9,	x9
+	addi	x9,	x9,	-1
+	beq	x8,	x8,	stop
+	sb	x10,	0x6e(x16)
 
 stop:
-	li	x8,	0xff000000
-	sw	x0,	0(x8)
-
-calc:
-	add	x4,	x1,	x2
-	jr	x3
+	li	x30,	0xff000000
+	sw	x0,	0x0(x30)
 
 	.data    # data section
 	.word	0x01028182
