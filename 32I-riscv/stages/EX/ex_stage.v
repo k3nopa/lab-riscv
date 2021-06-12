4a0bad49d6c8f957726202c90bed2f926d58a14e
k3nopa <afiqnaufalpro@gmail.com>
2021-06-12 19:48:55 +0900

    simplify the mux for alu

--- 32I-riscv/stages/EX/ex_stage.v
+++ 32I-riscv/stages/EX/ex_stage.v
@@ -14,29 +14,26 @@ module ex_stage (
 
     wire [31:0] alu_a, alu_b, test;
 
-    function [63:0] src_mux(
-        input alu_src_a, alu_src_b,
-        input [31:0] in_pc, in_a, in_b, in_sext
-    );
-        reg [31:0] _a, _b;
+    function [31:0] a_selector(input [31:0] in_a, input [31:0] in_b, input select);
         begin
-            case (alu_src_a)
-                0:_a = pc;
-                1:_a = in_a;
-                default : ;
+            case(select)
+                0:  a_selector = in_a; 
+                1:  a_selector = in_b; 
             endcase
-
-            case (alu_src_b)
-                0:_b = in_b;
-                1:_b = in_sext;
-                default : ;
-            endcase
-            
-            src_mux = {_a, _b};
         end
     endfunction
 
-    assign {alu_a, alu_b} = src_mux(alu_src_a, alu_src_b, pc, a, b, sext);
+    function [31:0] b_selector(input [31:0] in_a, input [31:0] in_b, input select);
+        begin
+            case(select)
+                0:  b_selector = in_a; 
+                1:  b_selector = in_b; 
+            endcase
+        end
+    endfunction
+
+    assign alu_a = a_selector(pc, a, alu_src_a);
+    assign alu_b = b_selector(b, sext, alu_src_b);
 
     ex_alu alu(.a(alu_a), .b(alu_b), .op(alu_op), .is_signed(is_signed), .result(alu_result), .branch(branch_result));
 
