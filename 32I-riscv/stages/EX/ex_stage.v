ce9a626775cee2cbe7e1e66d5d9cf853c3ac0139
k3nopa <afiqnaufalpro@gmail.com>
2021-06-15 02:34:46 +0900

    reduce function usage on mux

--- 32I-riscv/stages/EX/ex_stage.v
+++ 32I-riscv/stages/EX/ex_stage.v
@@ -12,28 +12,31 @@ module ex_stage (
     output [31:0]   alu_result
 );
 
-    wire [31:0] alu_a, alu_b, test;
+    wire [31:0] alu_a, alu_b;
 
-    function [31:0] a_selector(input [31:0] in_a, input [31:0] in_b, input select);
-        begin
-            case(select)
-                0:  a_selector = in_a; 
-                1:  a_selector = in_b; 
-            endcase
-        end
-    endfunction
+//    function [31:0] a_selector(input [31:0] in_a, input [31:0] in_b, input select);
+//        begin
+//            case(select)
+//                0:  a_selector = in_a; 
+//                1:  a_selector = in_b; 
+//            endcase
+//        end
+//    endfunction
+//
+//    function [31:0] b_selector(input [31:0] in_a, input [31:0] in_b, input select);
+//        begin
+//            case(select)
+//                0:  b_selector = in_a; 
+//                1:  b_selector = in_b; 
+//            endcase
+//        end
+//    endfunction
+//
+//    assign alu_a = a_selector(pc, a, alu_src_a);
+//    assign alu_b = b_selector(b, sext, alu_src_b);
 
-    function [31:0] b_selector(input [31:0] in_a, input [31:0] in_b, input select);
-        begin
-            case(select)
-                0:  b_selector = in_a; 
-                1:  b_selector = in_b; 
-            endcase
-        end
-    endfunction
-
-    assign alu_a = a_selector(pc, a, alu_src_a);
-    assign alu_b = b_selector(b, sext, alu_src_b);
+    assign alu_a = (alu_src_a) ? a : pc;
+    assign alu_b = (alu_src_b) ? sext : b;
 
     ex_alu alu(.a(alu_a), .b(alu_b), .op(alu_op), .is_signed(is_signed), .result(alu_result), .branch(branch_result));
 
