50ae4de39c01448532472dd159e30785819a687a
k3nopa <afiqnaufalpro@gmail.com>
2021-06-21 04:08:53 +0900

    rm unnecessary function usages

--- 32I-riscv/stages/IF/if_stage.v
+++ 32I-riscv/stages/IF/if_stage.v
@@ -13,18 +13,7 @@ module if_stage (
 
     wire [31:0] pc_in, current_pc;
 
-    function [31:0] mux;
-        input [31:0] _in1, _in2, _select;
-        begin
-            case (_select)
-                0 : mux = _in1;
-                1 : mux = _in2;
-                default: ;
-            endcase
-        end
-    endfunction
-
-    assign pc_in = (jump == 2'b10) ? jump_addr : mux(pc, branch_addr, pc_src);
+    assign pc_in = (jump == 2'd2) ? jump_addr : (pc_src) ? branch_addr : pc;
     assign current_pc = (stall == 1'b1) ? (pc_in - 4) : pc_in; 
     
     assign inst_addr = current_pc;
